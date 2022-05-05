ade76227f54b90f4c6af2669543aa69947fa2f3d
venkat-srinadh <yerravenkatsrinadhreddy@gmail.com>
2022-05-05 11:47:41 +0530

    commit

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -1,4 +1,4 @@
-var url = "./Resources/sheets/file-medium.xlsx";
+var url = "./Resources/sheets/file.xlsx";
 var oReq = new XMLHttpRequest();
 oReq.open("GET", url, true);
 oReq.responseType = "arraybuffer";
